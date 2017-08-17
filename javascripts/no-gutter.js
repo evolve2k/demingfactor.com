7998cb861a171f8a213be8d1b347d069f9f1af4a
lmkenny <lmkenly@gmail.com>
2017-08-17 11:54:52 +0930

    add Adelaide united and space between case studies

---
+++ javascripts/no-gutter.js
@@ -0,0 +1,13 @@
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  // options
+
+  itemSelector: '.box',
+  percentagePosition: 'true',
+
+});
+
+var msnry = new Masonry( '.grid', {
+  // options
+    
+});
